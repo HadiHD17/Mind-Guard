d4bd3efd0c32789e54eb4f499b7e23abd0cdee4f
HadiHD17 <hadihaidar1723@gmail.com>
2025-09-15 02:35:49 +0300

    HAD-27 fixed swagger responses

---
+++ Server/MindGuardServer/Helpers/CommonResponsesOperationFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class CommonResponsesOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        void Add(string code, string description)
+        {
+            if (!operation.Responses.ContainsKey(code))
+                operation.Responses[code] = new OpenApiResponse { Description = description };
+        }
+
+        Add("400", "Bad Request");
+        Add("401", "Unauthorized");
+        Add("403", "Forbidden");
+        Add("404", "Not Found");
+        Add("500", "Internal Server Error");
+    }
+}
