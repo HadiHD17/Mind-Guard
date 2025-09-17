df6aa491fab4f8b014f538957a154217cb0dabbc
HadiHD17 <hadihaidar1723@gmail.com>
2025-09-18 00:52:28 +0300

    fixed jest error

---
+++ Client/MindGuardClient/jest.config.js
@@ -0,0 +1,42 @@
+// jest.config.js
+module.exports = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(expo|@expo|react-native|@react-native|react-native-gesture-handler|react-native-reanimated|@react-native-async-storage/async-storage)/)",
+  ],
+
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native" +
+      "|@react-native" +
+      "|react-native-.*" +
+      "|@react-navigation" +
+      "|@react-native-async-storage" +
+      "|expo(nent)?" +
+      "|@expo(nent)?/.*" +
+      "|expo-modules-core" +
+      "|@unimodules/.*" +
+      "|unimodules-.*" +
+      "|sentry-expo" +
+      "|native-base" +
+      "|@react-native-community/.*" +
+      ")/)",
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "@expo/vector-icons": "<rootDir>/__mocks__/expo-vector-icons.js",
+    "@react-native-async-storage/async-storage":
+      "<rootDir>/__mocks__/async-storage.js",
+  },
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.test.{js,jsx,ts,tsx}",
+    "!**/__tests__/**",
+    "!**/__mocks__/**",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!jest.config.js",
+    "!babel.config.js",
+    "!metro.config.js",
+  ],
+};
